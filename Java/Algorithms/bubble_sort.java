9461d34d0b44e6a55531fba800f63ce5788aa977
Danil <danil.present@mail.ru>
2018-05-21 20:12:12 +0800

    Add input value array

--- Java/Algorithms/bubble_sort.java
+++ Java/Algorithms/bubble_sort.java
@@ -1,21 +1,49 @@
+package com.company;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class BubbleSort {
+public class Main {
+
+    public static Scanner s = new Scanner(System.in);
 
     public static void main(String[] args) {
-	    Scanner  s = new Scanner(System.in);
-		int[] arr = {5, 4, 7, 1, 7};
-		for (int i = arr.length - 1; i > 0 ; i--) {
-			for (int j = 0; j < i; j++) {
-				if (arr[j] > arr[j+1]) {
-					int t = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = t;
-		System.out.println(Arrays.toString(arr));
-				}
-			}
+        System.out.println("The addition of arrays");
+		System.out.println("");
+		System.out.print("Input lenght first array: ");
+        int t = s.nextInt();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+		System.out.print("Input value first array: ");
+        for (int i=0; i<t; i++) {
+            arr.add(s.nextInt());
+        }
+		System.out.print("Input lenght second array: ");
+		int t1 = s.nextInt();
+		ArrayList<Integer> arr1 = new ArrayList<Integer>();
+		System.out.print("Input value second array: ");
+		for (int i=0; i<t1; i++) {
+			arr1.add(s.nextInt());
+		}
+		System.out.println("");
+		System.out.println("Your 1 array:");
+		for (int i = 0; i < t; i++) {
+			System.out.println(i + ". " + arr.get(i));
+		}
+		System.out.println("");
+		System.out.println("Your 2 array:");
+		for (int i = 0; i < t1; i++) {
+			System.out.println(i + ". " + arr1.get(i));
+		}
+		System.out.println("");
+		System.out.println("Result:");
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		res.addAll(arr);
+		res.addAll(arr1);
+		Collections.sort(res);
+		for (int i = 0; i < res.size(); i++) {
+			System.out.println(i + ". " + res.get(i));
 		}
     }
 }
