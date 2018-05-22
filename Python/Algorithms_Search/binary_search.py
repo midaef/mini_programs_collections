d8a71d96a282bb9e6b86a7ffb8ea056ecd312179
Danil <danil.present@mail.ru>
2018-05-22 22:00:25 +0800

    Fix bug

--- Python/Algorithms_Search/binary_search.py
+++ Python/Algorithms_Search/binary_search.py
@@ -10,14 +10,16 @@ sr_value = len(arr) // 2
 
 while min_value < max_value:
 	sr_value = (max_value +  min_value) // 2
-	sr1_value = arr(sr_value)
+	sr1_value = arr[sr_value]
 	if sr1_value < search_value:
 		min_value = sr_value + 1
-	elif sr1_value > x:
+	elif sr1_value > search_value:
 		max_value =  sr_value
 	else:
-		return sr_value
+		print('Search: ' + str(search_value))
+		print('Status: Find!')
+		break
 		
 	
 else:
-	return -1
+	print('Status: Not find!')
